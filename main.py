c6c2261b5e99f25e44bd6b36b07107e91a697f8e
Raven K <85319728+ravenkmusic@users.noreply.github.com>
2022-01-07 20:36:48 +0000

    created new variables to make all strings the same case for accurate count of characters

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ name2 = input("What is their name? \n")
 
 #Write your code below this line 👇
 
+lower_name1 = name1.lower()
+lower_name2 = name2.lower()
+
+
+
+
+
 if score < 10 or score > 90:
-  print(f"Your score is **x**, you go together like coke and mentos.")
+  print(f"Your score is {score}, you go together like coke and mentos.")
 if score >= 40 and score < 50:
-  print(f"Your score is **x**, you go together like coke and mentos.")
+  print(f"Your score is {score}, you go together like coke and mentos.")
 else:
-  print(f"Your score is **z**.")
+  print(f"Your score is {score}.")
