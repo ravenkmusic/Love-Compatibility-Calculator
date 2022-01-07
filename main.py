5711dccda3e8d0f309e5fd9adb10206918b9957b
Raven K <85319728+ravenkmusic@users.noreply.github.com>
2022-01-07 20:55:04 +0000

    changed variable names

--- main.py
+++ main.py
@@ -10,24 +10,24 @@ lower_name1 = name1.lower()
 lower_name2 = name2.lower()
 
 name1_score = int(
-name1.count('t') +
-name1.count('r') +
-name1.count('u') +
-name1.count('e') +
-name1.count('l') +
-name1.count('o') +
-name1.count('v'))
+lower_name1.count('t') +
+lower_name1.count('r') +
+lower_name1.count('u') +
+lower_name1.count('e') +
+lower_name1.count('l') +
+lower_name1.count('o') +
+lower_name1.count('v'))
 
 print(name1_score)
 
 name2_score = int(
-name2.count('t') +
-name2.count('r') +
-name2.count('u') +
-name2.count('e') +
-name2.count('l') +
-name2.count('o') +
-name2.count('v'))
+lower_name2.count('t') +
+lower_name2.count('r') +
+lower_name2.count('u') +
+lower_name2.count('e') +
+lower_name2.count('l') +
+lower_name2.count('o') +
+lower_name2.count('v'))
 
 print(name2_score)
 
@@ -37,5 +37,5 @@ if score < 10 or score > 90:
   print(f"Your score is {score}, you go together like coke and mentos.")
 if score >= 40 and score < 50:
   print(f"Your score is {score}, you go together like coke and mentos.")
-else:
+elif:
   print(f"Your score is {score}.")
