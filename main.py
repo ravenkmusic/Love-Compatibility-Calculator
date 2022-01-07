3c43e38c2bb650a6d38e742ee8e7bb1f74ef13db
Raven K <85319728+ravenkmusic@users.noreply.github.com>
2022-01-07 20:47:52 +0000

    added new functions to calculate occurrences in strings

--- main.py
+++ main.py
@@ -9,9 +9,29 @@ name2 = input("What is their name? \n")
 lower_name1 = name1.lower()
 lower_name2 = name2.lower()
 
+name1_score = int(
+name1.count('t') +
+name1.count('r') +
+name1.count('u') +
+name1.count('e') +
+name1.count('l') +
+name1.count('o') +
+name1.count('v') +
+name1.count('e'))
 
+print(name1_score)
 
+name2_score = int(
+name2.count('t') +
+name2.count('r') +
+name2.count('u') +
+name2.count('e') +
+name2.count('l') +
+name2.count('o') +
+name2.count('v') +
+name2.count('e'))
 
+print(name2_score)
 
 if score < 10 or score > 90:
   print(f"Your score is {score}, you go together like coke and mentos.")
