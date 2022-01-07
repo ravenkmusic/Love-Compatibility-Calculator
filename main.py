08bebbdd485288e8ef933707a1428934493e372b
RavenKMusic <85319728+ravenkmusic@users.noreply.github.com>
2022-01-07 20:18:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if score < 10 or score > 90:
+  print(f"Your score is **x**, you go together like coke and mentos.")
+if score >= 40 and score < 50:
+  print(f"Your score is **x**, you go together like coke and mentos.")
+else:
+  print(f"Your score is **z**.")
